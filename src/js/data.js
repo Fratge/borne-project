c068ed118626a78e0ffd9216490eff4ee6f16376
SAYBOU <114362943+Fratge@users.noreply.github.com>
2023-09-05 10:23:30 -0400

    feat: adding data

---
+++ src/js/data.js
@@ -0,0 +1,63 @@
+const filmsList = [
+    {
+        id: 1,
+        filmName: "Blue Beetle",
+        price: 12,
+        filmImage: "./src/img/films/blue_beetle.jpeg"
+    },
+    {
+        id: 2,
+        filmName: "Barbie",
+        price: 12,
+        filmImage: "./src/img/films/barbie.jpeg"
+    },
+    {
+        id: 3,
+        filmName: "Gran Turismo",
+        price: 15,
+        filmImage: "./src/img/films/gran_turismo.jpeg"
+    },
+    {
+        id: 4,
+        filmName: "Oppenheimer",
+        price: 12,
+        filmImage: "./src/img/films/Oppenheimer.webp"
+    },
+    {
+        id: 5,
+        filmName: "Les hommes de ma mère",
+        price: 15,
+        filmImage: "./src/img/films/les_hommes_meres.jpeg"
+    },
+    {
+        id: 6,
+        filmName: "Le temps d'un été",
+        price: 18,
+        filmImage: "./src/img/films/temps_ete.avif"
+    },
+];
+
+const snacksList = [
+    {
+        id: 1,
+        snackName: "Cola",
+        smallFormat: 5,
+        largeFormat: 7,
+        snackImage: "./src/img/snacks/Cola.jpeg"
+    },
+    {
+        id: 2,
+        snackName: "Jus",
+        smallFormat: 3,
+        largeFormat: 5,
+        snackImage: "./src/img/snacks/jus-fruits-tropicana.png"
+    },
+    {
+        id: 3,
+        snackName: "Pop Corn",
+        smallFormat: 7,
+        largeFormat: 10,
+        snackImage: "./src/img/snacks/Cola.jpeg"
+    },
+]
+
