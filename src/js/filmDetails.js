7fe11ec41079ad4da6253dd84ffda84cf46923cd
Sébastien Gross <2357498@cgmatane.qc.ca>
2023-09-07 16:55:28 -0400

    feat: panier

--- src/js/filmDetails.js
+++ src/js/filmDetails.js
@@ -1,5 +1,23 @@
 const selectedFilm = JSON.parse(localStorage.getItem('selectedFilm'));
 
+const plusButton = document.getElementById("plusButton");
+const minusButton = document.getElementById("minusButton");
+const inputValue = document.getElementById("inputValue");
+
+plusButton.addEventListener("click", function() {
+  let currentValue = parseInt(inputValue.textContent);
+  currentValue += 1;
+  inputValue.textContent = currentValue;
+});
+
+minusButton.addEventListener("click", function() {
+  let currentValue = parseInt(inputValue.textContent);
+  if (currentValue > 1) {
+    currentValue -= 1;
+    inputValue.textContent = currentValue;
+}
+});
+
 if (selectedFilm) {
   filmId.innerText = selectedFilm.id ;
   filmName.innerText = selectedFilm.filmName;
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,63 @@
 const app = () => {
+    let initialPanier = JSON.parse(localStorage.getItem("panierUser")) || [];
+
     return {
         filmsList: filmsList,
         snacksList: snacksList,
+        quantity: 1, 
 
         redirectToFilmDetails(film){
             localStorage.setItem('selectedFilm', JSON.stringify(film));
             window.location.href = 'filmDetails.html';
+        },
+
+        panierUser: initialPanier,
+        selectedFilm: JSON.parse(localStorage.getItem('selectedFilm')),
+
+        addToPanier() {
+            const newItem = {
+                filmName: this.selectedFilm.filmName,
+                price: this.selectedFilm.price * this.quantity + ' €',
+                quantity: this.quantity,
+            };
+        
+            const existingItemIndex = this.panierUser.findIndex(item => item.filmName === newItem.filmName);
+        
+            if (existingItemIndex !== -1) {
+                this.panierUser[existingItemIndex].quantity += newItem.quantity;
+                this.panierUser[existingItemIndex].price = this.selectedFilm.price * this.panierUser[existingItemIndex].quantity + ' €';
+            } else {
+                this.panierUser.push(newItem);
+            }
+        
+            this.setPanierUser();
+          },
+
+        removeFromPanier(item) {
+            this.panierUser.splice(item, 1);
+            this.setPanierUser();
+        },
+
+        setPanierUser() {
+            localStorage.setItem("panierUser", JSON.stringify(this.panierUser));
+            console.log(this.panierUser)
+        },
+
+        calculateTotal() {
+            let total = 0;
+            for (const item of this.panierUser) {
+                const price = parseFloat(item.price);
+                if (!isNaN(price)) {
+                    total += price;
+                }
+            }
+            return total.toFixed(2);
+        },
+
+        isMany(item){
+            const filmName = item.filmName;
+            const count = this.panierUser.filter(item => item.filmName === filmName).length;
+            return count;
         }
     }
 }
