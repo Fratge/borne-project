9095732ef137a048c5736a503c735b3fc6278c04
themightdodo <quentin.bovi@laposte.net>
2023-09-11 15:16:58 -0400

    définition de style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,25 @@ module.exports = {
         sans:[
             'Dosis, sans-serif',
         ]
+      },
+      fontSize:{
+        titre1:['128px','auto'],
+        titre2:['40px','auto'],
+        titre3:['24px','auto'],
+        titre4:['20px','auto'],
+        paragraphe:['16px','auto'],
+      },
+      fontWeight: {
+        thin: '100',
+        hairline: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
     },
     },
   },
