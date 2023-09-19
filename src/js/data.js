7c1a673574254269abac1fc111c0226b72d78236
themightdodo <quentin.bovi@laposte.net>
2023-09-19 10:22:49 -0400

    data ajout horaire synopsis

--- src/js/data.js
+++ src/js/data.js
@@ -5,7 +5,9 @@ const filmsList = [
         adultePrice: 12,
         etudiantPrice: 8,
         enfantPrice: 4,
-        filmImage: "./src/img/films/blue_beetle.jpeg"
+        filmImage: "./src/img/films/blue_beetle.jpeg",
+        Synopsis : "Jaime Reyes est un jeune plein d'ambition. Alors qu'il rentre de l'université, le Scarabée, une ancienne relique d'une biotechnologie extraterrestre choisit de faire de lui son hôte. Ce dernier se voit alors revêtu d'une armure hors du commun qui lui octroie des pouvoirs extraordinaires et imprévisibles. Tout bascule alors pour Jaime qui devient le super-héros Blue Beetle.",
+        Horaire: "16h30 à 18h00"
     },
     {
         id: 2,
@@ -13,7 +15,9 @@ const filmsList = [
         adultePrice: 12,
         etudiantPrice: 8,
         enfantPrice: 3,
-        filmImage: "./src/img/films/barbie.jpeg"
+        filmImage: "./src/img/films/barbie.jpeg",
+        Synopsis: "Barbie, qui vit à Barbie Land, est expulsée du pays pour être loin d'être une poupée à l'apparence parfaite; n'ayant nulle part où aller, elle part pour le monde humain et cherche le vrai bonheur.",
+        Horaire: "18h00 à 19h30"
     },
     {
         id: 3,
@@ -21,7 +25,9 @@ const filmsList = [
         adultePrice: 15,
         etudiantPrice: 8,
         enfantPrice: 2,
-        filmImage: "./src/img/films/gran_turismo.jpeg"
+        filmImage: "./src/img/films/gran_turismo.jpeg",
+        Synopsis: "La jeune Jann Mardenborough meurt d'envie d'être pilote de course, mais n'a pas les moyens financiers de faire partie du sport d'élite. C'est pourquoi il n'a appuyé sur l'accélérateur que dans le jeu vidéo Gran Turismo et a connu un énorme succès.",
+        Horaire: "20h00 à 21h30"
     },
     {
         id: 4,
@@ -29,7 +35,8 @@ const filmsList = [
         adultePrice: 12,
         etudiantPrice: 8,
         enfantPrice: 2,
-        filmImage: "./src/img/films/Oppenheimer.webp"
+        filmImage: "./src/img/films/Oppenheimer.webp",
+        Synopsis:"Pendant la Seconde Guerre mondiale, le lieutenant-général Leslie Groves Jr. nomme le physicien J. Robert Oppenheimer pour travailler sur le projet ultra-secret Manhattan. Oppenheimer et une équipe de scientifiques passent des années à développer et à concevoir la bombe atomique.",
     },
     {
         id: 5,
@@ -37,7 +44,9 @@ const filmsList = [
         adultePrice: 15,
         etudiantPrice: 8,
         enfantPrice: 6,
-        filmImage: "./src/img/films/les_hommes_meres.jpeg"
+        filmImage: "./src/img/films/les_hommes_meres.jpeg",
+        Synopsis:"La jeune Elsie est surprise par les dernières volontés de sa mère excentrique: retrouver ses cinq ex-maris pour disperser ses cendres. Elsie est déterminée à honorer la mémoire de sa mère, mais le voyage change profondément sa trajectoire.",
+        Horaire: "21h00 à 22h00"
     },
     {
         id: 6,
@@ -45,7 +54,10 @@ const filmsList = [
         adultePrice: 18,
         etudiantPrice: 8,
         enfantPrice: 4,
-        filmImage: "./src/img/films/temps_ete.jpg"
+        filmImage: "./src/img/films/temps_ete.jpg",
+        Synopsis:"Depuis plus de 25 ans, Marc Côté, aumônier de rue et curé de paroisse, vit avec les pauvres et les itinérants. Épuisé de tenir à bout de bras son église qui sert de refuge, stressé par les comptes qu'il n'arrive plus à payer, Marc doit se rendre à l'évidence: il devra fermer le lieu. Comme un appel de la Providence, il hérite d'une propriété dans le Bas-du-Fleuve et décide d'y emmener avec lui une bande de sans-abri, des hommes et des femmes qui, tout comme lui, ont besoin de vacances.",
+        Horaire: "14h00 à 16h00"
+
     },
 ];
 
